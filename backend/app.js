08f8999a50eb18477658eb2f3cefa83802bb6ca6
Agustin Gonzalez <agustin.gonzalez@example.com>
2026-06-17 17:08:08 -0300

    configurar servidor express inicial

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        mensaje: "API funcionando correctamente"
+    });
+});
+
+module.exports = app;
